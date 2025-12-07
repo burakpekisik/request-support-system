ad6026cb5f3f0ccd0f2facc2b0b665133959ecd6
Ali Burak Pekışık <burakpekisik@gmail.com>
2025-12-07 14:16:31 +0300

    first commit

---
+++ src/main/java/com/ceng454/request_support_system/RequestSupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ceng454.request_support_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestSupportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestSupportSystemApplication.class, args);
+	}
+
+}
